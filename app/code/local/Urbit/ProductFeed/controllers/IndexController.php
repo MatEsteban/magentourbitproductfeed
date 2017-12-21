1555117074cfd6fe1230ef1ddf734af0bedc1992
Unknown <mathieu@urbit.com>
2017-12-21 14:58:07 +0100

    4th filter

--- app/code/local/Urbit/ProductFeed/controllers/IndexController.php
+++ app/code/local/Urbit/ProductFeed/controllers/IndexController.php
@@ -17,13 +17,60 @@ class Urbit_ProductFeed_IndexController extends Mage_Core_Controller_Front_Actio
     public function IndexAction()
     {
 	    /** @var Urbit_ProductFeed_Model_Config $config */
-        $config = Mage::getModel("productfeed/config");
+        
+        $config = $this->getConfig();
+
+        //get product_id filter's value from config 
+        $configProductFilterValue = isset($config->filter['product_id']) ? $config->filter['product_id'] : null;
+        
+       
+        //ajax response for Left Multiselect
+        if ($this->getRequest()->getPost('ajax_left')) {
+
+            $tags = $this->getRequest()->getPost('tags');
+            $categories = $this->getRequest()->getPost('collects');
+            $stock = $this->getRequest()->getPost('stock');
+
+            $filterArray = array(
+                'category' => $categories ? implode(',', $categories) : '',
+                'tag' => $tags ? implode(',', $tags) : '',
+                'stock' => $stock ? : '',
+                'product_id' => '',
+            );
+
+            $productCollection = $this->getFilteredProductCollection($filterArray);
+            $optionsArray = $this->getOptionArray($productCollection);
+
+            $this->getResponse()->setHeader('Content-type', 'application/json');
+            $this->getResponse()->setBody(json_encode($optionsArray));
+            
+            return;
+        }
+
+        //ajax response for Right Multiselect
+        if ($this->getRequest()->getPost('ajax_right')) { 
+
+            if ($configProductFilterValue) {
+                $filterArray = $this->getFilterArrayForProductId($config);
+
+                $productCollection = $this->getFilteredProductCollection($filterArray);
+                $optionsArray = $this->getOptionArray($productCollection);
+
+                $this->getResponse()->setHeader('Content-type', 'application/json');
+                $this->getResponse()->setBody(json_encode($optionsArray));
+            }
+
+            return;
+        }
+
+        $filterArray = $configProductFilterValue ? 
+            $this->getFilterArrayForProductId($config) : $config->filter;
+        
+        $this->_products = Mage::getModel("productfeed/list_product", $filterArray);
 
         /**
          * @var Urbit_ProductFeed_Model_List_Product
          */
-        $this->_products = Mage::getModel("productfeed/list_product", $config->filter);
-        
         $this->getResponse()
             ->clearHeaders()
             ->setHeader('Content-Type', 'application/json', true)
@@ -45,5 +92,44 @@ class Urbit_ProductFeed_IndexController extends Mage_Core_Controller_Front_Actio
         }
 
         return $feedHelper->getDataJson();
+    }
+
+    //return array of products filtered by $filterArray
+    public function getFilteredProductCollection($filterArray) {
+        $products = Mage::getModel("productfeed/list_product", $filterArray);
+
+        return $products->getCollection()->getItems();
+    }
+
+    //return options for multiselect
+    public function getOptionArray($collection) 
+    {
+        $optionsArray = array();
+
+        foreach ($collection as $product) {
+            $optionsArray[] = array(
+                'id'    => $product->getId(),
+                'name'  => $product->load($product->getId())->getName(),
+            );
+        }
+
+        return $optionsArray;
+    }
+
+    public function getFilterArrayForProductId($config)
+    {
+        $config = $this->getConfig();
+
+        return array(
+            'category' => '',
+            'tag' => '',
+            'stock' => '',
+            'product_id' => $config->filter['product_id'] ? : '',
+        );
+    }
+
+    public function getConfig()
+    {
+        return Mage::getModel("productfeed/config");
     }
 }
